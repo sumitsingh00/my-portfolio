f1b3ba77d89e40db15d77067c6d20b28f3e3abd4
sumit singh <83850096+sumitsingh00@users.noreply.github.com>
2024-01-03 16:24:49 +0530

    Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -57,7 +57,7 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 {/* <span className="tagline">Welcome to my Portfolio</span> */}
                 <h1>{`Hi! I'm sumit`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Frontend Developer", "backend Developer", "ML Developer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Full stack Developer | 5 ⭐ HackerRank | React JS |Node JS | DBMS | SQL | java |C++| data structure</p>
+                  <p>MERN stack Developer | 5 ⭐ HackerRank | React JS |Express JS | DBMS | SQL | java |C++| data structure</p>
                   {/* <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button> */}
               </div>}
             </TrackVisibility>
